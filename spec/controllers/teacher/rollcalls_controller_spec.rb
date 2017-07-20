6e1165843462666eb5924fa23448b1ff6313c9a7
xanderflood <xander.t.flood@gmail.com>
2017-07-20 07:14:45 -0400

    Removing index route specs

--- spec/controllers/teacher/rollcalls_controller_spec.rb
+++ spec/controllers/teacher/rollcalls_controller_spec.rb
@@ -41,14 +41,6 @@ RSpec.describe Teacher::RollcallsController, type: :controller do
   # Teacher::RollcallsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      rollcall = Rollcall.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
   describe "GET #show" do
     it "returns a success response" do
       rollcall = Rollcall.create! valid_attributes
