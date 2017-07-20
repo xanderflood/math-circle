e6d214a163c2a1c9f513000dcf4e2526f05551d7
xanderflood <xander.t.flood@gmail.com>
2017-07-20 07:09:38 -0400

    Didn't commit schema after creating rollcall scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716202505) do
+ActiveRecord::Schema.define(version: 20170716234042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,15 @@ ActiveRecord::Schema.define(version: 20170716202505) do
     t.index ["reset_password_token"], name: "index_parents_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "rollcalls", force: :cascade do |t|
+    t.integer  "event_id"
+    t.text     "attendance"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_rollcalls_on_event_id", using: :btree
+  end
+
   create_table "semesters", force: :cascade do |t|
     t.string   "name"
     t.date     "start"
@@ -157,4 +166,5 @@ ActiveRecord::Schema.define(version: 20170716202505) do
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "rollcalls", "events"
 end
