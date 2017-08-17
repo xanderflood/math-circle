330e0d31a6b5f0e98ad9c5e97afdc1d3fcc4cbf5
xanderflood <xander.t.flood@gmail.com>
2017-08-17 19:04:00 -0400

    LotteryError now (ironically) records all unhandled errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from StandardError, with: :log_error
+
   def configure_permitted_parameters
     update_attrs = [:password, :password_confirmation, :current_password]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  end
+
+  def log_error(e)
+    LotteryError.save!(e)
+
+    # do the usual Rails error stuff anyways, I guess
+    raise
   end
 end
