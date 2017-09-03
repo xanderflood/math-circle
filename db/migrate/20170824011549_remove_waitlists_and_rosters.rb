7e81b5366cb6765fee5b24bfc4ce3f7d1e453b01
xanderflood <xander.t.flood@gmail.com>
2017-09-03 11:34:53 -0400

    Updated create_registrees migration to convert the rosters and waitlists

--- db/migrate/20170824011549_remove_waitlists_and_rosters.rb
+++ db/migrate/20170824011549_remove_waitlists_and_rosters.rb
@@ -1,5 +1,28 @@
 class RemoveWaitlistsAndRosters < ActiveRecord::Migration[5.0]
-  def change 
+  def change
+    Course.each do |course|
+      course.section.each do |section|
+        section.roster.each do |student|
+          Registree.create!(
+            semester: c.semester,
+            student: student,
+            course: course,
+            section: section)
+        end
+      end
+
+      course.waitlist.each do |student|
+        ballot = student.ballot
+
+        Registree.create!(
+          semester: c.semester,
+          student: student,
+          course: course,
+          section: "waitlist",
+          preferences: ballot.preferences)
+      end
+    end
+
     remove_column :courses, :waitlist, :text
 
     remove_column :event_groups, :waitlist, :text
