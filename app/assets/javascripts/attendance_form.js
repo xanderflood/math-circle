1c63ff2d1b501cce2f3c7a1a7f0c40a313663be9
xanderflood <xander.t.flood@gmail.com>
2017-09-15 19:57:54 -0400

    Controls for extra rows in attendance

--- app/assets/javascripts/attendance_form.js
+++ app/assets/javascripts/attendance_form.js
@@ -1,4 +1,9 @@
+// Set up a storage place for row models
+window.attendanceForm = window.attendanceForm || {}
+
 $(function() {
+  // // // First, set up  all the JQuery callbacks // // //
+  // Process data from attendance form and submit it
   $(".attendance-form .actions input").click(function(e) {
     var table = $(e.target);
 
@@ -19,4 +24,30 @@ $(function() {
 
     return true;
   });
+
+  // Add unregistered row
+  $("table.rollcall-table tbody.controls button.new-unregistered").click(function(e) {
+    $("table.rollcall-table tbody.extras").append(
+      window.attendanceForm.unregisteredModel.clone(true));
+  });
+
+  // Add unexpected row
+  $("table.rollcall-table tbody.controls button.new-unexpected").click(function(e) {
+    $("table.rollcall-table tbody.extras").append(
+      window.attendanceForm.unexpectedModel.clone(true));
+  });
+
+  // Delete row
+  $("table.rollcall-table tbody.extras a.delete-row").click(function(e) {
+    $(this).closest("tr.attendance-row").remove();
+  });
+
+  // // // THEN, copy the models, this way they have the necessary events // // //
+  modelGroup = $("table.rollcall-table tbody.extras.models");
+  window.attendanceForm.unexpectedModel   = modelGroup.find(".unexpected-row");
+  window.attendanceForm.unregisteredModel = modelGroup.find(".unregistered-row");
+
+  window.attendanceForm.unexpectedModel.detach();
+  window.attendanceForm.unregisteredModel.detach();
+  modelGroup.remove();
 });
