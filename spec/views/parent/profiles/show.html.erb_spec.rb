85fb75b6fd80b6faff0e475141268db5991b913b
xanderflood <xander.t.flood@gmail.com>
2017-09-30 09:25:55 -0400

    Starting to do serious rspec cleanup

--- spec/views/parent/profiles/show.html.erb_spec.rb
+++ spec/views/parent/profiles/show.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "parent/profile/edit", type: :view do
+RSpec.describe "parent/profile/show", type: :view do
   before(:each) do
     @parent_profile = assign(:parent_profile, create(:parent_profile))
   end
 
-  it "renders the edit parent_profile form" do
+  it "renders the show parent_profile form" do
     render
 
     assert_select "form[action=?][method=?]", parent_profile_path(@parent_profile), "post" do
--- spec/views/teacher/registrees/edit.html.erb_spec.rb
+++ spec/views/teacher/registrees/edit.html.erb_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "teacher/registrees/edit", type: :view do
   before(:each) do
-    @teacher_registree = assign(:teacher_registree, Registree.create!())
+    @registree = assign(:registree, Registree.where.not(section: nil).limit(1).first)
   end
 
   it "renders the edit teacher_registree form" do
     render
 
-    assert_select "form[action=?][method=?]", teacher_registree_path(@teacher_registree), "post" do
+    assert_select "form[action=?][method=?]", teacher_student_registree_path(@registree), "post" do
     end
   end
 end
