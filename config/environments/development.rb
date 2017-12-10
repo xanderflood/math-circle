4bbf21b9a1bc0fa464c97f7daee53b5cfde2eab8
xanderflood <xander.t.flood@gmail.com>
2017-12-10 09:11:43 -0500

    Moved letter_opener to development-only config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,3 +53,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+LetterOpener.configure do |config|
+  # To overrider the location for message storage.
+  # Default value is <tt>tmp/letter_opener</tt>
+  config.location = Rails.root.join('tmp', 'my_mails')
+
+  # To render only the message body, without any metadata or extra containers or styling.
+  # Default value is <tt>:default</tt> that renders styled message with showing useful metadata.
+  config.message_template = :light
+end
