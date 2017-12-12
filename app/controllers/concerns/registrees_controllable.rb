59d9305bf23455386702538aceed76a04451e4b7
Xander Flood <xander.t.flood@gmail.com>
2017-12-12 13:11:42 -0500

    Refactored the two ballots_controller's and registree_controller's into concerns
    The concerns are role-agnostic so there's no further code duplication between the descendants of Parent::BaseController and Teacher::BaseController. Going to roll this pattern out to (almost?) all the other controllers.

---
+++ app/controllers/concerns/registrees_controllable.rb
@@ -0,0 +1,59 @@
+module RegistreesControllable
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :role
+
+    before_action :set_student
+    before_action :set_registree, only: [:show, :edit, :update, :destroy]
+  end
+
+  def new
+    @registree = Registree.new(student: @student, semester: @semester)
+  end
+
+  def edit
+  end
+
+  def create
+    @registree = Registree.new(registree_params)
+
+    if @registree.save
+      redirect_to [self.class.role, :students], notice: 'Registree was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @registree.update(registree_params)
+      redirect_to [self.class.role, :students], notice: 'Registree was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @registree.destroy
+    redirect_to [self.class.role, :students], notice: 'Registree was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_registree
+      @registree = @student.registree
+    end
+
+    def set_student
+      @student = Student.find(student_id)
+    end
+
+    def student_id
+      @student_id ||= params[:student_id] || params[:id]
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def registree_params
+      params.require(:registree).permit(:student_id, :semester_id, :course_id, :section_id, :student_id, preferences_hash: (1..Ballot::MAX_PREFERENCES).map(&:to_s))
+    end
+end
--- app/controllers/parent/ballots_controller.rb
+++ app/controllers/parent/ballots_controller.rb
@@ -1,72 +1,13 @@
 class Parent::BallotsController < Parent::BaseController
-  before_action :set_ballot,  only: [:edit, :update, :destroy]
-  before_action :set_student, only: [:edit, :new]
+  include BallotsControllable
+  self.role = :parent
 
-  rescue_from Ballot::NoCoursesError, with: :no_courses
-  rescue_from Ballot::NoLevelError,   with: :no_level
-
-  def new
-    if @ballot
-      redirect_to edit_parent_student_ballot_path(@student)
-    end
-
-    if @student.school_grade < 6 || @student.level == 'D'
-      redirect_to :back, notice: 'Elementary students and students registering for level D will need to contact Math-Circle directly to register.'
-    end
-
-    @ballot = Ballot.new(semester: Semester.current, student_id: @student.id)
-  end
-
-  def edit
-  end
-
-  def create
-    @ballot = Ballot.new(ballot_params)
-
-    if @ballot.save
-      redirect_to parent_students_path, notice: 'Ballot was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @ballot.update(ballot_params)
-      redirect_to parent_students_path, notice: 'Ballot was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @ballot.destroy
-    redirect_to parent_students_path, notice: 'Ballot was successfully removed.'
-  end
+  before_action :check_levels, only: :new
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ballot
-      @ballot = Ballot.where(student_id: student_id, semester: Semester.current).limit(1).first
-    end
-
-    def set_student
-      @student = Student.find(student_id)
-    end
-
-    def student_id
-      params[:student_id] || params[:id]
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def ballot_params
-      params.fetch(:ballot, {}).permit(:semester_id, :student_id, :course_id, preferences_hash: (1..Ballot::MAX_PREFERENCES).map(&:to_s))
-    end
-
-    def no_courses
-      redirect_to :back, notice: 'No courses are currently scheduled for this Math-Circle level.'
-    end
-
-    def no_level
-      redirect_to :back, notice: 'You must specify a Math-Circle level for this student before registering.'
+    def check_levels
+      if @student.school_grade < 6 || @student.level == 'D'
+        redirect_to :back, notice: 'Elementary students and students registering for level D will need to contact Math-Circle directly to register.'
+      end
     end
 end
