cfcc4b8aad875c9ee1060bf3e9f094fdd8403c6b
xanderflood <xander.t.flood@gmail.com>
2018-01-07 14:56:12 -0500

    Refactored and robustified search code

--- app/controllers/teacher/parents_controller.rb
+++ app/controllers/teacher/parents_controller.rb
@@ -1,22 +1,12 @@
 class Teacher::ParentsController < Teacher::BaseController
   before_action :set_parent, only: [:show, :destroy]
 
+  include Searchable
+  search :parent_profiles, :profiles
+
   def index
     @profiles = ParentProfile.order(:first_name, :last_name)
                 .paginate(page: params[:page], per_page: 50)
-  end
-
-  def search
-    @profiles = if params[:search][:id]
-      p = ParentProfile.find_by_id(params[:search][:id].to_i)
-
-      [p].compact
-    else
-      ParentProfile.where("first_name ILIKE ?", "%#{params[:search][:first_name]}%")
-      .where("last_name ILIKE ?",  "%#{params[:search][:last_name]}%")
-      .where("email ILIKE ?",  "%#{params[:search][:email]}%")
-      .where("id ILIKE ?",  "%#{params[:search][:id]}%")
-    end
   end
 
   def destroy
