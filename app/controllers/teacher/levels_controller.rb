f58373c3602f4ef80264f0a7556b8ef90e13bd4c
xanderflood <xander.t.flood@gmail.com>
2018-09-04 11:25:29 -0400

    some levels_controller logic"

--- app/controllers/teacher/levels_controller.rb
+++ app/controllers/teacher/levels_controller.rb
@@ -4,6 +4,15 @@ class Teacher::LevelsController < Teacher::BaseController
   end
 
   def update
-    # TODO
+    levels = JSON.parse(params["levels"])
+
+    Level.transaction do
+      levels.each do |l|
+        #TODO new stuff
+        Level.update(l)
+      end
+    end
+
+    redirect_to [:teacher, :levels], notice: "Changes have been saved."
   end
 end
