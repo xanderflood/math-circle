a65cb34fd913361f500e4b3eb7487e4fb27d1061
xanderflood <xander.t.flood@gmail.com>
2018-09-11 08:07:58 -0400

    remove JS logs

--- app/assets/javascripts/course_select.js
+++ app/assets/javascripts/course_select.js
@@ -1,17 +1,14 @@
 window.courseSelect = window.courseSelect || (function() {
   var _go = function(e) {
-    console.log("hey!!!");
     link = $(this);
     urlTemplate = link.data().urlTemplate;
     courseID = $("select#course_id :selected").val();
 
-    console.log(courseID);
     if (courseID.length != 0) {
       url = urlTemplate.replace("courseID", courseID);
 
       //allow the default action to take us there
       link.attr("href", url);
-      console.log(url);
     } else {
       return false
     }
