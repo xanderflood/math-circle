b4777e3c4d926c4a31e474db3bc55e3c8ebbf8f3
xanderflood <xander.t.flood@gmail.com>
2018-11-09 20:24:30 -0500

    do all the resets when a new semester is published

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -24,18 +24,24 @@ class Semester < ApplicationRecord
     # TODO: put email callbacks here
 
     before_transition [:lottery_closed, :lottery_done] => :lottery_done, do: :commit_lottery
-    before_transition                             all:    :closed,       do: :reset_all_levels
+    before_transition hidden: :lottery_open, do: :reset_for_publish
 
-    event(:publish) { transition         hidden:    :lottery_open }
-
-    event(:close_lottery) { transition                    lottery_open:    :lottery_closed }
-
-    event(:run) { transition [:lottery_closed, :lottery_done] => :lottery_done }
-
-    event(:open_registration) { transition lottery_done: :registration_open,
-                                                 closed: :registration_open }
-
-    event(:close_registration) { transition registration_open: :closed }
+    event(:publish) {
+      transition hidden: :lottery_open
+    }
+    event(:close_lottery) {
+      transition lottery_open: :lottery_closed
+    }
+    event(:run) {
+      transition [:lottery_closed, :lottery_done] => :lottery_done
+    }
+    event(:open_registration) {
+      transition lottery_done: :registration_open,
+                       closed: :registration_open
+    }
+    event(:close_registration) {
+      transition registration_open: :closed
+    }
   end
 
   LOTTERY_STATES = ["lottery_closed", "lottery_done", "lottery_open"]
@@ -89,14 +95,10 @@ class Semester < ApplicationRecord
 
   protected
   ### callbacks ###
-  def reset_all_levels
-    Student.update_all(level: :unspecified)
+  def reset_for_publish
+    Student.update_all(level: :unspecified, grade: nil)
+    Semester.where.not(state: [:hidden, :closed]).update(state: :closed)
   end
-
-  # TODO: this should be a validation, when a semseter gets published
-  # def ensure_all_other_semesters_are_closed
-  #   Semester.all.all? { |s| s.closed? } # returns false unless all semesters are closed
-  # end
 
   def end_after_start
     errors.add(:end, 'must be later than the start date.') if self.end <= self.start
